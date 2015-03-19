5bb9de50387134860a4aa3e785b9923dbc8f89c0
Nathan Friedly <nathan@nfriedly.com>
2015-03-19 14:50:41 -0400

    Adding ConceptInsights.semanticSearch example to show correct API usage.
    [ci skip]

--- examples/concept_insights.v1.js
+++ examples/concept_insights.v1.js
@@ -8,6 +8,10 @@ var concept_insights = watson.concept_insights({
   version: 'v1'
 });
 
+///////////////////
+// Annotate text //
+///////////////////
+
 var params = {
   user: 'wikipedia',
   graph: 'en-20120601',
@@ -20,4 +24,28 @@ concept_insights.annotateText(params, function(err, res) {
     console.log(err);
   else
     console.log(JSON.stringify(res, null, 2));
+});
+
+
+/////////////////////
+// Semantic Search //
+/////////////////////
+
+var payload = {
+  func: 'semanticSearch',
+  ids: [
+    '/graph/wikipedia/en-20120601/Software_development_process',
+    '/graph/wikipedia/en-20120601/Programming_tool'
+  ],
+  corpus: 'ibmresearcher',
+  user: 'public',
+  limit: 5
+
+};
+
+concept_insights.semanticSearch(payload, function(error, results) {
+  if (error)
+    console.log(error);
+  else
+    console.log(JSON.stringify(results, null, 2));
 });
