00721f6581fceb375b566b07c31e6c771d6be82f
Nathan Friedly <nathan@nfriedly.com>
2015-12-16 13:17:17 -0500

    added links, don't include generated index.html in the directory listing

--- jsdoc/generate_index_html.sh
+++ jsdoc/generate_index_html.sh
@@ -3,8 +3,10 @@
 # based on https://odoepner.wordpress.com/2012/02/17/shell-script-to-generate-simple-index-html/
 
 echo '<!DOCTYPE html><html><body>'
-echo '<h1>JSDoc for <a href="https://npmjs.org/package/watson-developer-cloud"><code>watson-developer-cloud</code></a></h1>'
-echo '<p>See also: <a href="https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/">https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/</a></p>'
-echo '<ul>'
-ls | sed 's/^.*/<li><a href="&">&<\/a><\/li>/'
+echo '<h1>JSDoc for IBM Watson Developer Cloud Node.js SDK</h1>'
+echo '<p><a href="https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/">docs</a>'
+echo ' | <a href="https://github.com/watson-developer-cloud/node-sdk">GitHub</a>'
+echo ' | <a href="https://npmjs.org/package/watson-developer-cloud">npm</a>'
+echo '</p><ul>'
+ls| grep --invert-match index.html | sed 's/^.*/<li><a href="&">&<\/a><\/li>/'
 echo '</ul></body></html>'
