5ed632fb56c8e4db6fdca553154716990adf6a67
Nathan Friedly <nathan@nfriedly.com>
2015-12-16 12:34:50 -0500

    auto doc publishing improvements: * only run on first job of a given build (instead of publishing the same docs 3 times) * run for both tagged and regular builds * include commit hash that triggered build in gh-pages commit message

--- jsdoc/publish.sh
+++ jsdoc/publish.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # based on http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
-# todo: add    && [ "$TRAVIS_TAG" != "" ] after confirming it works otherwise
 
-if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_TAG" ]; then
+# checking the build/job numbers allows it to only publish once even though we test against multiple node.js versions
+
+if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" ] && [ "$TRAVIS_BUILD_NUMBER.1" == "$TRAVIS_JOB_NUMBER" ]; then
 
   echo "Publishing JSDoc..."
 
@@ -12,16 +13,21 @@ if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] && [ "$TRAVIS_PU
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/watson-developer-cloud/node-sdk gh-pages > /dev/null
 
   pushd gh-pages
-    cp -Rf ../doc/watson-developer-cloud/* ./
+    # on tagged builds, $TRAVIS_BRANCH is the tag (e.g. v1.2.3), otherwise it's the branch name (e.g. master)
+    rm -rf $TRAVIS_BRANCH
+    mkdir $TRAVIS_BRANCH
+    cp -Rf ../doc/watson-developer-cloud/*/* ./$TRAVIS_BRANCH
     git add -f .
-    git commit -m "JSDdoc for $TRAVIS_TAG"
+    git commit -m "JSDdoc for $TRAVIS_BRANCH ($TRAVIS_COMMIT)"
     git push -fq origin gh-pages > /dev/null
   popd
 
-  echo -e "Published JSDoc for $TRAVIS_TAG to gh-pages.\n"
+  # todo: whip up a quick index file that links to all subdirs
+
+  echo -e "Published JSDoc for $TRAVIS_BRANCH to gh-pages.\n"
 
 else
 
-  echo -e "Not publishing docs for tag $TRAVIS_TAG on branch $TRAVIS_BRANCH of repo $TRAVIS_REPO_SLUG"
+  echo -e "Not publishing docs for build $TRAVIS_BUILD_NUMBER ($TRAVIS_JOB_NUMBER) on branch $TRAVIS_BRANCH of repo $TRAVIS_REPO_SLUG"
 
 fi
