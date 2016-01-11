d2479ea881a12e077241135c8d933edd633cf55c
Melissa Mahoney <mmahone@us.ibm.com>
2016-01-11 16:31:13 -0600

    Adding VR node examples

---
+++ examples/resources/visual_recognition.v2-beta-examples/visual_recognition.v2-beta-listclassifiers.js
@@ -0,0 +1,17 @@
+var watson = require('watson-developer-cloud');
+
+var visual_recognition = watson.visual_recognition({
+  username: '09b559ae-066f-49b2-b48b-eae7e2dc558f',
+  password: 'VX11OLZ4uplF',
+  version: 'v2-beta',
+  version_date: '2015-12-02'
+});
+
+visual_recognition.listClassifiers({},
+	function(err, response) {
+	 if (err)
+		console.log(err);
+	 else
+		console.log(JSON.stringify(response, null, 2));
+	}
+);
