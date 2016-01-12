a0f2b4fb713340ca07217833c583ed6fd61c699c
Nathan Friedly <nathan@nfriedly.com>
2016-01-12 11:24:55 -0500

    adding a touch of style to the jsdoc index page

--- jsdoc/generate_index_html.sh
+++ jsdoc/generate_index_html.sh
@@ -2,12 +2,31 @@
 
 # based on https://odoepner.wordpress.com/2012/02/17/shell-script-to-generate-simple-index-html/
 
-echo '<!DOCTYPE html><html><body>'
-echo '<h1>IBM Watson Developer Cloud Node.js SDK</h1>'
-echo '<p><a href="https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/">Documentation</a>'
-echo ' | <a href="https://github.com/watson-developer-cloud/node-sdk">GitHub</a>'
-echo ' | <a href="https://npmjs.org/package/watson-developer-cloud">npm</a>'
-echo '</p>'
-echo '<p>JSDoc by branch/tag:</p><ul>'
+echo '<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IBM Watson Developer Cloud</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="page-header">
+        <h1>IBM Watson Developer Cloud Node.js SDK</h1>
+    </div>
+
+    <p><a href="http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/">Info</a>
+        | <a href="http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/">Documentation</a>
+        | <a href="https://github.com/watson-developer-cloud/node-sdk">GitHub</a>
+        | <a href="https://npmjs.org/package/watson-developer-cloud">npm</a>
+    </p>
+
+    <p>JSDoc by branch/tag:</p>
+    <ul>'
 ls | grep --invert-match index.html | sed 's/^.*/<li><a href="&">&<\/a><\/li>/'
-echo '</ul></body></html>'
+echo '    </ul>
+</div>
+</body>
+</html>'
