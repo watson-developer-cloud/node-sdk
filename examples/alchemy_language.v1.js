c5f701fc8b3cf1b55a9d07b5a0087d98a88d2eaf
Garrett May <gamay@us.ibm.com>
2016-07-14 15:24:55 -0400

    alchemylanguage node examples

--- examples/alchemy_language.v1.js
+++ examples/alchemy_language.v1.js
@@ -1,16 +1,252 @@
 'use strict';
 
 var watson = require('watson-developer-cloud');
-
 var alchemy_language = watson.alchemy_language({
-  api_key: '<api_key>'
+  api_key: 'API_KEY'
 });
 
-var params = {
+// Combined call
+var parameters = {
+  extract: 'entities,keywords',
+  sentiment: 1,
+  maxRetrieve: 1,
+  url: 'https://www.ibm.com/us-en/'
+};
+
+alchemy_language.combined(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Authors (using example.html from the same local directory)
+var fs = require('fs');
+var html;
+var parameters = {};
+fs.readFile('example.html', 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  parameters = {
+    'html': data,
+    'showSourceText': 1,
+  };
+  alchemy_language.authors(parameters, function (error, response) {
+    if (error)
+      console.log('error:', error);
+    else
+      console.log(JSON.stringify(response, null, 2));
+  })
+});
+
+
+// Concepts
+var parameters = {
+  url: 'http://www.ibm.com/watson',
+  knowledgeGraph: 1
+};
+
+alchemy_language.concepts(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Date Extraction
+var parameters = {
+  text: 'Set a reminder for my appointment next Tuesday',
+  anchorDate: '2016-03-22 00:00:00'
+};
+
+alchemy_language.dates(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+
+// Emotion Analysis
+var parameters = {
+  url: 'http://www.charliechaplin.com/en/synopsis/articles/29-The-Great-Dictator-s-Speech'
+};
+
+alchemy_language.emotion(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Entities
+var parameters = {
+  url: 'http://www-03.ibm.com/press/us/en/pressrelease/49384.wss'
+};
+
+alchemy_language.entities(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Feed Detection
+var parameters = {
+  url: 'http://news.ycombinator.com'
+};
+
+alchemy_language.feeds(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Keywords
+var parameters = {
+  url: 'http://www.twitter.com/ibmwatson'
+};
+
+alchemy_language.feeds(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Language Detection
+var parameters = {
+  url: 'http://www.ibm.com/us-en/'
+};
+
+alchemy_language.language(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Microformats
+var parameters = {
+  url: 'http://www.ibm.com/us-en/'
+};
+
+alchemy_language.microformats(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Publication date
+var parameters = {
+  url: 'https://www.whitehouse.gov/the-press-office/2016/03/19/weekly-address-president-obamas-supreme-court-nomination'
+};
+
+alchemy_language.publicationDate(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Relations
+var parameters = {
+  url: 'https://www.whitehouse.gov/the-press-office/2016/03/19/weekly-address-president-obamas-supreme-court-nomination'
+};
+
+alchemy_language.relations(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Typed Relations
+var parameters = {
+  url: 'http://techcrunch.com'
+};
+
+alchemy_language.relations(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+
+// Sentiment
+var parameters = {
   text: 'IBM Watson won the Jeopardy television show hosted by Alex Trebek'
 };
 
-alchemy_language.sentiment(params, function (err, response) {
+alchemy_language.sentiment(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Targeted Sentiment
+var parameters = {
+  text: 'IBM Watson won the Jeopardy television show hosted by Alex Trebek',
+  targets: [
+    'IBM Watson'
+  ]
+};
+
+alchemy_language.sentiment(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Taxonomy
+var parameters = {
+  url: 'cnn.com'
+};
+
+alchemy_language.taxonomy(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Text extraction (cleaned)
+var parameters = {
+  url: 'http://techcrunch.com/2016/01/29/ibm-watson-weather-company-sale'
+};
+
+alchemy_language.text(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Text extraction (raw)
+var parameters = {
+  url: 'http://techcrunch.com/2016/01/29/ibm-watson-weather-company-sale',
+  raw: true
+};
+
+alchemy_language.text(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+// Title extraction
+var parameters = {
+  url: 'http://techcrunch.com/2016/01/29/ibm-watson-weather-company-sale'
+};
+
+alchemy_language.title(parameters, function (err, response) {
   if (err)
     console.log('error:', err);
   else
