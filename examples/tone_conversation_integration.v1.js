aaf650e2009e08e01535c25a2727a36a2aa6c147
Vibha Sinha <vibha.sinha@us.ibm.com>
2016-08-05 14:59:56 -0700

    minor changes

--- examples/tone_conversation_integration.v1.js
+++ examples/tone_conversation_integration.v1.js
@@ -3,21 +3,21 @@ var watson = require('watson-developer-cloud');
 var tone_detection = require("./conversation_addons/tone_detection.js");
 
 var conversation = watson.conversation({
-  username: '49ab433a-f379-4b67-86f2-950715f473a9',
-  password: 'lG2qAe0DcERY',
+  username: '<username>',
+  password: '<password>',
   version: 'v1',
   version_date: '2016-07-01'
 });
 
  var tone_analyzer = watson.tone_analyzer({
-   username: '5a440129-bfbe-4bcc-9d3f-621a26ad4318',
-   password: 'UCXw5ULTWP46',
+   username: '<username>',
+   password: '<password>',
    version_date: '2016-05-19',
    version: 'v3'
 });
   
  var payload = {
-    workspace_id: '7c6f674e-a387-45d6-aa11-73474d3a3350',
+    workspace_id: '<workspace id>',
     input: 'I am not happy today :(',
     context: {}
  };
