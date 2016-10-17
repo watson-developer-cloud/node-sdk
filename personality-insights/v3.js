18d6a698785a5bdde64206abd0792fa06acbff81
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-10-17 10:12:18 -0400

    Update example and jsdoc

--- personality-insights/v3.js
+++ personality-insights/v3.js
@@ -47,14 +47,15 @@ PersonalityInsightsV3.URL = 'https://gateway.watsonplatform.net/personality-insi
 /**
  * @param {Object} params The parameters to call the service
  * @param {Object} [params.headers] - The header parameters.
- * @param {string} [params.headers.accept-language=en] - The desired language of the response: ar (Arabic), en (English), es (Spanish), or ja (Japanese).
+ * @param {string} [params.headers.accept-language=en] - The desired language of the response.
  * @param {string} [params.headers.content-type=text/plain] - The content type of the request: text/plain (the default), text/html, or application/json.
  * @param {string} [params.headers.content-language=en] - The language of the input text for the request: ar (Arabic), en (English), es (Spanish), or ja (Japanese)
  * @param {string} [params.headers.accept=application/json] - The desired content type of the response: application/json (the default) or text/csv
  * @param {string} [params.text] - The text to analyze.
  * @param {Object} [params.content_items] - A JSON input (if 'text' not provided).
  * @param {boolean} [params.raw_scores=false] - include raw results.
- * @param {boolean} [params.consumption_preferences=true] - If true, information about consumption preferences is returned with the results.
+ * @param {boolean} [params.csv_headers=false] - If true, column labels are returned with a CSV response; if false (the default), they are not. Applies only when the Accept header is set to text/csv.
+ * @param {boolean} [params.consumption_preferences=false] - If true, information about consumption preferences is returned with the results.
  *
  * @param callback The callback.
  */
