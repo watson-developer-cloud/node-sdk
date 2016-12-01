bade7f53003f3a2e496da215948cbffc840ec073
Nathan Friedly <nathan@nfriedly.com>
2016-12-01 11:20:05 -0500

    Added discovery to readme, version_date constant, jsdoc

--- examples/discovery.v1-experimental.js
+++ examples/discovery.v1-experimental.js
@@ -7,7 +7,7 @@ var discovery = new DiscoveryV1Experimental({
   // environment properties, and then Bluemix's VCAP_SERVICES environment property
   //username: 'INSERT YOUR USERNAME FOR THE SERVICE HERE',
   //password: 'INSERT YOUR PASSWORD FOR THE SERVICE HERE'
-  version_date: '2016-11-17'
+  version_date: DiscoveryV1Experimental.VERSION_DATE_2016_07_11
 });
 
 discovery.getEnvironments({}, function(error, data) {
