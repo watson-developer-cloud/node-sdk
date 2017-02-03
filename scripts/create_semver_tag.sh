523681456b02bd00927aaae7faaa3c1e98dfd5a7
Nathan Friedly <nathan@nfriedly.com>
2017-02-03 17:35:10 -0500

    switching form colon to space to better match ci skip command [semver minor]

--- scripts/create_semver_tag.sh
+++ scripts/create_semver_tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Looks for [semver:major], [semver:minor], and [semver:patch] in the commit message, creates the appropriate tag when found
+# Looks for [semver major], [semver minor], and [semver patch] in the commit message, creates the appropriate tag when found
 
 # pushing back to github will cause a new travis build to release the tag
 
@@ -10,7 +10,7 @@ if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] \
   && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
   && [ "$TRAVIS_BRANCH" == "master" ] \
   && [ "$TRAVIS_BUILD_NUMBER.1" == "$TRAVIS_JOB_NUMBER" ] \
-  && [[ "$TRAVIS_COMMIT_MESSAGE" =~ \[semver:(major|minor|patch)\] ]] \
+  && [[ "$TRAVIS_COMMIT_MESSAGE" =~ \[semver\ (major|minor|patch)\] ]] \
   ; then
 
   export SEMVER=${BASH_REMATCH[1]};
@@ -25,7 +25,7 @@ if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] \
 
 else
 
-  echo -e "Not creating a semver tag for build $TRAVIS_JOB_NUMBER on branch $TRAVIS_BRANCH of repo $TRAVIS_REPO_SLUG with commit message:"
+  echo -e "Not creating a semver tag for build $TRAVIS_JOB_NUMBE on branch $TRAVIS_BRANCH of repo $TRAVIS_REPO_SLUG with commit message:"
   echo $TRAVIS_COMMIT_MESSAGE
 
 fi
