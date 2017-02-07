8ef485ff78ea837de886d1a6330ff44d3a5d938b
Nathan Friedly <nathan@nfriedly.com>
2017-02-07 09:20:29 -0500

    organising semver tag script [ci skip]

--- scripts/create_semver_tag.sh
+++ scripts/create_semver_tag.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Looks for [semver major], [semver minor], and [semver patch] in the commit message, creates the appropriate tag when found
-
-# pushing back to github will cause a new travis build to release the tag
+# Looks for [semver major], [semver minor], and [semver patch] in the commit message,
+# and run npm version {version} to bump and tag if found.
+# Then pushes back to github to cause a new travis build that will publish the new version.
 
 # checking the build/job numbers allows it to only publish once even though we test against multiple node.js versions
 # The ".1" job is the first on the list in .travis.yml. By convention, this is the oldest supported node.js version.
-if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] \
+
+#config
+export SLUG="watson-developer-cloud/node-sdk"
+export BRANCH="master"
+
+
+if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] \
   && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
-  && [ "$TRAVIS_BRANCH" == "master" ] \
+  && [ "$TRAVIS_BRANCH" == "$BRANCH" ] \
   && [ "$TRAVIS_BUILD_NUMBER.1" == "$TRAVIS_JOB_NUMBER" ] \
   && [[ "$TRAVIS_COMMIT_MESSAGE" =~ \[semver\ (major|minor|patch)\] ]] \
   ; then
@@ -16,14 +22,15 @@ if [ "$TRAVIS_REPO_SLUG" == "watson-developer-cloud/node-sdk" ] \
   export SEMVER=${BASH_REMATCH[1]};
   echo "Creating semver $SEMVER per commit message"
 
-  git checkout master
+  # checkout the branch that the commit is actually on instead of "detached head" state
+  git checkout $BRANCH
 
   npm version $SEMVER
 
   git config --global push.default simple
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git config remote.origin.url https://${GH_TOKEN}@github.com/watson-developer-cloud/node-sdk
+  git config remote.origin.url https://${GH_TOKEN}@github.com/${SLUG}
   git push --follow-tags
 
 else
