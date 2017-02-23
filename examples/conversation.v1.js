647e9be986ade162eec6bde844d9f08c396b193a
David Boaz <davidbo@il.ibm.com>
2017-02-23 18:20:07 +0200

    conversation example with two messages

--- examples/conversation.v1.js
+++ examples/conversation.v1.js
@@ -13,24 +13,45 @@ const conversation = new watson.ConversationV1({
 });
 
 /**
- * Payload for the Watson Conversation Service
- * <workspace-id> and user input text required.
+ * Conversation service is stateless.
+ * The application need to maintain the conversation state.
+ * A context object is returned in the messgage response and the app should pass it in the next message request
  */
-const payload = {
-  workspace_id: process.env.WORKSPACE_ID || '<workspace_id>',
-  input: {
-    text: 'I am happy to talk to you today.'
-  }
+let currentContext = undefined;
+
+const message = function(text) {
+  const payload = {
+    workspace_id: process.env.WORKSPACE_ID || '<workspace_id>',
+    input: {
+      text: text
+    },
+    context: currentContext
+  };
+  return new Promise((resolve, reject) => conversation.message(payload, function(err, data) {
+    if (err) {
+      reject(err);
+    } else {
+      // remember the conversation state
+      currentContext = data.context;
+      resolve(data);
+    }
+  }));
 };
 
-conversation.message(payload, function(err, data) {
-  if (err) {
-    // APPLICATION-SPECIFIC CODE TO PROCESS THE ERROR
-    // FROM CONVERSATION SERVICE
-    console.error(JSON.stringify(err, null, 2));
-  } else {
-    // APPLICATION-SPECIFIC CODE TO PROCESS THE DATA
-    // FROM CONVERSATION SERVICE
-    console.log(JSON.stringify(data, null, 2));
-  }
+message('first message').then(resp1 => {
+  // APPLICATION-SPECIFIC CODE TO PROCESS THE DATA
+  // FROM CONVERSATION SERVICE
+  console.log(JSON.stringify(resp1, null, 2));
+
+  // invoke a second call to conversation
+  message('second message')
+    .then(resp2 => {
+      console.log(JSON.stringify(resp2, null, 2));
+      console.log('Note that the two reponses have the same context.conversation_id');
+    })
+    .catch(err => {
+      // APPLICATION-SPECIFIC CODE TO PROCESS THE ERROR
+      // FROM CONVERSATION SERVICE
+      console.error(JSON.stringify(err, null, 2));
+    });
 });
