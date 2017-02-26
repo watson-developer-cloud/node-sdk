5f7199d27672a3a6940522b4cd8ce34537d41513
David Boaz <davidbo@il.ibm.com>
2017-02-26 07:52:32 +0200

    conversation example - improvements...

--- examples/conversation.v1.js
+++ examples/conversation.v1.js
@@ -13,51 +13,45 @@ const conversation = new watson.ConversationV1({
 });
 
 /**
- * Conversation service is stateless.
- * The application need to maintain the conversation state.
- * A context object is returned in the messgage response and the app should pass it in the next message request
- */
-let currentContext = undefined;
-
-/**
  * Calls the conversation message api. 
  * returns a promise
  */
-const message = function(text) {
+const message = function(text, context) {
   const payload = {
     workspace_id: process.env.WORKSPACE_ID || '<workspace_id>',
     input: {
       text: text
     },
-    // pass the context from previous response
-    context: currentContext
+    context: context
   };
   return new Promise((resolve, reject) => conversation.message(payload, function(err, data) {
     if (err) {
       reject(err);
     } else {
-      // remember the conversation state
-      currentContext = data.context;
       resolve(data);
     }
   }));
 };
 
 // This example makes two successive calls to conversation service.
-message('first message').then(response1 => {
-  // APPLICATION-SPECIFIC CODE TO PROCESS THE DATA
-  // FROM CONVERSATION SERVICE
-  console.log(JSON.stringify(response1, null, 2));
+// Note how the context is passed:
+// In the first message the context is undefined. The service starts a new conversation.
+// The context returned from the first call is passed in the second request - to continue the conversation.
+message('first message', undefined)
+  .then(response1 => {
+    // APPLICATION-SPECIFIC CODE TO PROCESS THE DATA
+    // FROM CONVERSATION SERVICE
+    console.log(JSON.stringify(response1, null, 2), '\n--------');
 
-  // invoke a second call to conversation
-  message('second message')
-    .then(response2 => {
-      console.log(JSON.stringify(response2, null, 2));
-      console.log('Note that the two reponses have the same context.conversation_id');
-    })
-    .catch(err => {
-      // APPLICATION-SPECIFIC CODE TO PROCESS THE ERROR
-      // FROM CONVERSATION SERVICE
-      console.error(JSON.stringify(err, null, 2));
-    });
-});
+    // invoke a second call to conversation
+    return message('second message', response1.context);
+  })
+  .then(response2 => {
+    console.log(JSON.stringify(response2, null, 2), '\n--------');
+    console.log('Note that the two reponses should have the same context.conversation_id');
+  })
+  .catch(err => {
+    // APPLICATION-SPECIFIC CODE TO PROCESS THE ERROR
+    // FROM CONVERSATION SERVICE
+    console.error(JSON.stringify(err, null, 2));
+  });
