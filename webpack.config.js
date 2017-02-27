d91d887285a72796b0b99f94811a4213fe0f1b14
Nathan Friedly <nathan@nfriedly.com>
2017-02-27 09:03:59 -0500

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// With this configuration, the entire library can be bundled with Webpack. 
+// With this configuration, the entire library can be bundled with Webpack.
 // However, not all services support client-side usage.
 // See https://github.com/watson-developer-cloud/node-sdk/tree/master/examples/webpack for details
 
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'watson-developer-cloud.js'
+    filename: 'watson-developer-cloud.js',
     library: 'WatsonDeveloperCloud',
     libraryTarget: 'umd'
   },
