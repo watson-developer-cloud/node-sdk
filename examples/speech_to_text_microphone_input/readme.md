ad1776fe649a526e12ec7e3ab6a4912312d49185
Nathan Friedly <nathan@nfriedly.com>
2017-07-06 16:18:51 -0400

    speech to text microphone input examples

---
+++ examples/speech_to_text_microphone_input/readme.md
@@ -0,0 +1,5 @@
+# IBM Watson Speech to Text Microphone Transcription Example
+
+Uses IBM Watson and Node.js to transcribe speech captured from the computer's microphone.
+
+This example requires [arecord](http://alsa-project.org/) (Linux) or [sox](http://sox.sourceforge.net/) (Mac/Windows).
---
+++ examples/speech_to_text_microphone_input/transcribe-mic-to-console.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ silent: true }); // optional, handy for local development
+var SpeechToText = require('watson-developer-cloud/speech-to-text/v1');
+var LineIn = require('line-in'); // the `mic` package also works - it's more flexible but requires a bit more setup
+var wav = require('wav');
+
+
+var speechToText = new SpeechToText({
+  // if left unspecified here, the SDK will fall back to the SPEECH_TO_TEXT_USERNAME and SPEECH_TO_TEXT_PASSWORD
+  // environment properties, and then Bluemix's VCAP_SERVICES environment property
+  // username: 'INSERT YOUR USERNAME FOR THE SERVICE HERE',
+  // password: 'INSERT YOUR PASSWORD FOR THE SERVICE HERE'
+});
+
+var lineIn = new LineIn(); // 2-channel 16-bit little-endian signed integer pcm encoded audio @ 44100 Hz
+
+var wavStream = new wav.Writer({
+  sampleRate: 44100,
+  channels: 2
+});
+
+var recognizeStream = speechToText.createRecognizeStream({'content_type': 'audio/wav'});
+
+lineIn.pipe(wavStream);
+
+wavStream.pipe(recognizeStream);
+
+recognizeStream.pipe(process.stdout);
