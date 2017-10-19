e77c24e25a86c326c7ee975b8ceb24249a6c89c3
Ammar Dodin <ammar.dodin@ibm.com>
2017-10-19 15:08:48 -0400

    :new: generated tone-analyzer code

--- tone-analyzer/v3.js
+++ tone-analyzer/v3.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 IBM Corp. All Rights Reserved.
+ * Copyright 2017 IBM All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,114 +17,104 @@
 'use strict';
 
 const extend = require('extend');
-const pick = require('object.pick');
 const requestFactory = require('../lib/requestwrapper');
+const helper = require('../lib/helper');
 const util = require('util');
 const BaseService = require('../lib/base_service');
 
 /**
- * Tone Analyzer
  * @param {Object} options
+ * @param {String} options.version_date - Release date of the API version in YYYY-MM-DD format.
  * @constructor
  */
 function ToneAnalyzerV3(options) {
   BaseService.call(this, options);
-  // Check if 'version_date' was provided
+  // check if 'version_date' was provided
   if (typeof this._options.version_date === 'undefined') {
-    throw new Error('Argument error: version_date was not specified, use 2016-05-19');
+    throw new Error('Argument error: version_date was not specified, use ');
   }
-  // todo: confirm that the service wants version, not version_date
-  this._options.qs.version = this._options.version_date;
+  this._options.qs.version = options.version_date;
 }
 util.inherits(ToneAnalyzerV3, BaseService);
 ToneAnalyzerV3.prototype.name = 'tone_analyzer';
 ToneAnalyzerV3.prototype.version = 'v3';
 ToneAnalyzerV3.URL = 'https://gateway.watsonplatform.net/tone-analyzer/api';
 
+
 /**
- * Main API call. Returns the different tone dimensions of a text.
+ * Analyze general purpose tone.
  *
- * @param params: An object with a string 'text' element.
+ * Uses the general purpose endpoint to analyze the tone of your input content. The service analyzes the content for emotional and language tones. The method always analyzes the tone of the full document; by default, it also analyzes the tone of each individual sentence of the content.   You can submit no more than 128 KB of total input content and no more than 1000 individual sentences in JSON, plain text, or HTML format. The service analyzes the first 1000 sentences for document-level analysis and only the first 100 sentences for sentence-level analysis.   Use the `POST` request method to analyze larger amounts of content in any of the available formats. Use the `GET` request method to analyze smaller quantities of plain text content.   Per the JSON specification, the default character encoding for JSON content is effectively always UTF-8; per the HTTP specification, the default encoding for plain text and HTML is ISO-8859-1 (effectively, the ASCII character set). When specifying a content type of plain text or HTML, include the `charset` parameter to indicate the character encoding of the input text; for example: `Content-Type: text/plain;charset=utf-8`. For `text/html`, the service removes HTML tags and analyzes only the textual content.   **Note:** The `tones` query parameter is no longer supported. The service continues to accept the parameter for backward-compatibility, but the parameter no longer affects the response.
  *
- * Additional arguments on `params` object are optional, they include:
- *
- * - `tones`: A Comma-separated list of tones. Only these will be computed
- *    in this API call. Allowed values are 'social', 'emotion', 'language'.
- *    It defaults to 'all of them'.
- * - `sentences`: A boolean, telling if sentence-level analysis (which is
- *    slower than just document-level) needs to be computed. It defaults
- *    to true.
- * - `isHTML`: A boolean value telling that the `params.text` argument is
- *    to be trated as HTML contents. On HTML input, the services does
- *    cleanup of tags and performs the analysis on the text contents only.
- * - `language`: Language of the input. It defaults to `en`.
- *
- * @return upon success, the callback function is called with an object
- * containing the different tones (emotion, writing and social) analyzed.
- *
- * @see the API docs for a the full documentation.
- *
+ * @param {Object} params - The parameters to send to the service.
+ * @param {ToneInput} params.tone_input - JSON, plain text, or HTML input that contains the content to be analyzed. For JSON input, provide an object of type `ToneInput`.
+ * @param {string} params.content_type - The type of the input: application/json, text/plain, or text/html. A character encoding can be specified by including a `charset` parameter. For example, 'text/plain;charset=utf-8'.
+ * @param {boolean} [params.sentences] - Indicates whether the service is to return an analysis of each individual sentence in addition to its analysis of the full document. If `true` (the default), the service returns results for each sentence.
+ * @param {string} [params.content_language] - The language of the input text for the request: English or French. Regional variants are treated as their parent language; for example, `en-US` is interpreted as `en`. The input content must match the specified language. Do not submit content that contains both languages. You can specify any combination of languages for `content_language` and `Accept-Language`.
+ * @param {string} [params.accept_language] - The desired language of the response. For two-character arguments, regional variants are treated as their parent language; for example, `en-US` is interpreted as `en`. You can specify any combination of languages for `Content-Language` and `accept_language`.
+ * @param {Function} [callback] - The callback that handles the response.
  */
 ToneAnalyzerV3.prototype.tone = function(params, callback) {
-  if (!params || !params.text) {
-    callback(new Error('Missing required parameters: text'));
+  params = params || {};
+  const requiredParams = ['tone_input', 'content_type'];
+  const missingParams = helper.getMissingParams(params, requiredParams);
+  if (missingParams) {
+    callback(missingParams);
     return;
   }
-  const contentType = params.isHTML ? 'text/html' : 'text/plain';
-
-  const headers = {
-    accept: 'application/json',
-    'content-type': contentType
-  };
-
-  if (params.language) {
-    headers['content-language'] = params.language;
-  }
-
+  const body = params.tone_input;
+  const query = { sentences: params.sentences };
   const parameters = {
     options: {
       url: '/v3/tone',
       method: 'POST',
-      body: params.text,
-      qs: pick(params, ['tones', 'sentences'])
+      json: (params.content_type === 'application/json'),
+      body: body,
+      qs: query,
     },
     defaultOptions: extend(true, this._options, {
-      headers: headers
+      headers: {
+        'accept': 'application/json',
+        'content-type': params.content_type
+      }
     })
   };
-
   return requestFactory(parameters, callback);
 };
 
 /**
- * @param {Object} params The parameters to call the service
- * @param {Object} [params.utterances] - The utterances to analyze.  Utterances must be a JSON object.
+ * Analyze customer engagement tone.
  *
- * @param callback The callback.
+ * Use the customer engagement endpoint to analyze the tone of customer service and customer support conversations. For each utterance of a conversation, the method reports the most prevalent subset of the following seven tones: sad, frustrated, satisfied, excited, polite, impolite, and sympathetic.   If you submit more than 50 utterances, the service returns a warning for the overall content and analyzes only the first 50 utterances. If you submit a single utterance that contains more than 500 characters, the service returns an error for that utterance and does not analyze the utterance. The request fails if all utterances have more than 500 characters.   Per the JSON specification, the default character encoding for JSON content is effectively always UTF-8.
+ *
+ * @param {Object} params - The parameters to send to the service.
+ * @param {Utterance[]} params.utterances - An array of `Utterance` objects that provides the input content that the service is to analyze.
+ * @param {string} [params.accept_language] - The desired language of the response. For two-character arguments, regional variants are treated as their parent language; for example, `en-US` is interpreted as `en`.
+ * @param {Function} [callback] - The callback that handles the response.
  */
-ToneAnalyzerV3.prototype.tone_chat = function(params, callback) {
-  // For backward compatibility
-  if (params && params.utterances && params.utterances.utterances) {
-    params.utterances = params.utterances.utterances;
+ToneAnalyzerV3.prototype.toneChat = function(params, callback) {
+  params = params || {};
+  const requiredParams = ['utterances'];
+  const missingParams = helper.getMissingParams(params, requiredParams);
+  if (missingParams) {
+    callback(missingParams);
+    return;
   }
-
+  const body = { utterances: params.utterances };
   const parameters = {
-    requiredParams: ['utterances'],
-    originalParams: params,
     options: {
       url: '/v3/tone_chat',
       method: 'POST',
       json: true,
-      body: pick(params, ['utterances'])
+      body: body,
     },
     defaultOptions: extend(true, this._options, {
       headers: {
-        accept: 'application/json',
+        'accept': 'application/json',
         'content-type': 'application/json'
       }
     })
   };
-
   return requestFactory(parameters, callback);
 };
 
