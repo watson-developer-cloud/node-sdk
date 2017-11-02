8137565dce3fafbb26a6d0ab4f079b3f6eb7ef3e
Glenn R. Fisher <glennrfisher@gmail.com>
2017-11-02 18:56:11 -0500

    Add handwritten wrapper to avoid breaking changes

---
+++ natural-language-classifier/v1.js
@@ -0,0 +1,78 @@
+"use strict";
+/**
+ * Copyright 2017 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var GeneratedNaturalLanguageClassifierV1 = require("./v1-generated");
+var NaturalLanguageClassifierV1 = /** @class */ (function (_super) {
+    __extends(NaturalLanguageClassifierV1, _super);
+    function NaturalLanguageClassifierV1(options) {
+        return _super.call(this, options) || this;
+    }
+    /**
+     * Creates a classifier
+     */
+    NaturalLanguageClassifierV1.prototype.create = function (params, callback) {
+        // TODO
+    };
+    /**
+     * Returns the classification information for a classifier on a phrase
+     */
+    NaturalLanguageClassifierV1.prototype.classify = function (params, callback) {
+        params = params || {};
+        if (!params.classifier_id) {
+            params.classifier_id = params.classifier;
+        }
+        _super.prototype.classify.call(this, params, callback);
+    };
+    /**
+     * Returns the training status of the classifier
+     */
+    NaturalLanguageClassifierV1.prototype.status = function (params, callback) {
+        params = params || {};
+        if (!params.classifier_id) {
+            params.classifier_id = params.classifier;
+        }
+        _super.prototype.getClassifier.call(this, params, callback);
+    };
+    /**
+     * Retrieves the list of classifiers for the user
+     */
+    NaturalLanguageClassifierV1.prototype.list = function (params, callback) {
+        params = params || {};
+        _super.prototype.listClassifiers.call(this, params, callback);
+    };
+    /**
+     * Deletes a classifier
+     */
+    NaturalLanguageClassifierV1.prototype.remove = function (params, callback) {
+        params = params || {};
+        if (!params.classifier_id) {
+            params.classifier_id = params.classifier;
+        }
+        _super.prototype.deleteClassifier.call(this, params, callback);
+    };
+    return NaturalLanguageClassifierV1;
+}(GeneratedNaturalLanguageClassifierV1));
+module.exports = NaturalLanguageClassifierV1;
---
+++ natural-language-classifier/v1.ts
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2017 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Request = require('request'); 
+import extend = require('extend');
+import requestFactory = require('../lib/requestwrapper');
+import helper = require('../lib/helper');
+import util = require('util');
+import BaseService = require('../lib/base_service');
+import GeneratedNaturalLanguageClassifierV1 = require('./v1-generated');
+
+class NaturalLanguageClassifierV1 extends GeneratedNaturalLanguageClassifierV1 {
+
+  constructor(options) {
+    super(options);
+  }
+
+  /**
+   * Creates a classifier
+   */
+  create(params, callback) {
+    // TODO
+  }
+
+  /**
+   * Returns the classification information for a classifier on a phrase
+   */
+  classify(params, callback) {
+    params = params || {};
+    if (!params.classifier_id) {
+      params.classifier_id = params.classifier;
+    }
+    super.classify(params, callback);
+  }
+
+  /**
+   * Returns the training status of the classifier
+   */
+  status(params, callback) {
+    params = params || {};
+    if (!params.classifier_id) {
+      params.classifier_id = params.classifier;
+    }
+    super.getClassifier(params, callback);
+  }
+
+  /**
+   * Retrieves the list of classifiers for the user
+   */
+   list(params, callback) {
+     params = params || {};
+     super.listClassifiers(params, callback);
+   }
+
+  /**
+   * Deletes a classifier
+   */
+  remove(params, callback) {
+    params = params || {};
+    if (!params.classifier_id) {
+      params.classifier_id = params.classifier;
+    }
+    super.deleteClassifier(params, callback);
+  }
+}
+
+export = NaturalLanguageClassifierV1;
