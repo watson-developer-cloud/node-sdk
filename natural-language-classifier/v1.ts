c9a65cbef40b474a32be0d380f5777725b0256dd
Glenn R. Fisher <glennrfisher@gmail.com>
2017-11-17 08:42:48 -0600

    Add natural language classifier adapter

---
+++ natural-language-classifier/v1.ts
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2017 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import GeneratedNaturalLanguageClassifierV1 = require('./v1-generated');
+import extend = require('extend');
+import { getMissingParams } from '../lib/helper';
+import * as toCSV from './json-training-to-csv';
+import * as isStream from 'isstream';
+import * as omit from 'object.omit';
+
+class NaturalLanguageClassifierV1 extends GeneratedNaturalLanguageClassifierV1 {
+  
+  constructor(options) {
+    super(options);
+  }
+
+  create(params, callback) {
+    if (!params || !params.training_data) {
+      callback(new Error('Missing required parameters: training_data'));
+      return;
+    }
+
+    if (!(Array.isArray(params.training_data) || typeof params.training_data === 'string' || isStream(params.training_data))) {
+      callback(new Error('training_data needs to be a String, Array or Stream'));
+      return;
+    }
+
+    toCSV(params.training_data, (err, csv) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      const _params: GeneratedNaturalLanguageClassifierV1.CreateClassifierParams = {
+        metadata: Buffer.from(JSON.stringify(omit(params, ['training_data'])), 'utf8'),
+        training_data: csv
+      }
+
+      return super.createClassifier(_params, callback);
+    });
+  }
+
+  classify(params, callback) {
+    let _params = extend({}, params);
+    if (!_params.classifier_id) {
+      _params.classifier_id = _params.classifier;
+    }
+    return super.classify(_params, callback);
+  }
+
+  status(params, callback) {
+    let _params = extend({}, params);
+    if (!_params.classifier_id) {
+      _params.classifier_id = _params.classifier;
+    }
+    return super.getClassifier(_params, callback);
+  }
+
+  list(params, callback) {
+    return super.listClassifiers(params, callback);
+  }
+
+  remove(params, callback) {
+    let _params = extend({}, params);
+    if (!_params.classifier_id) {
+      _params.classifier_id = _params.classifier;
+    }
+    return super.deleteClassifier(_params, callback);
+  }
+}
+
+export = NaturalLanguageClassifierV1;
