34219d50208b8d4395b6f1701b5712768389c2c5
Ammar Dodin <ammar.dodin@ibm.com>
2017-11-20 23:46:15 -0500

    :unamused: refactor visual recognition adapter

--- visual-recognition/v3.ts
+++ visual-recognition/v3.ts
@@ -68,15 +68,14 @@ class VisualRecognitionV3 extends GeneratedVisualRecognitionV3 {
     return (a || b) && !(a && b);
   }
 
-  private parseParameters(params, callback) {
-    const _callback = callback ? callback : () => {};
+  private checkVisionParams(params) {
     const err = new Error(
       'Watson VisualRecognition.classify() requires either an images_file or a url parameter'
     );
     if (!params) {
-      return _callback(err);
+      throw err;
     }
-    params.images_file = params.images_file || params.image_file;
+    const _images_file = params.images_file || params.images_file;
     // need a try/catch for parameters since it can be stringified JSON
     // or it can be null if users are using old parameter names
     let _parameters;
@@ -87,75 +86,84 @@ class VisualRecognitionV3 extends GeneratedVisualRecognitionV3 {
     }
     if (
       !(
-        this.xor(params.images_file, params.url) ||
-        this.xor(params.images_file, _parameters.url)
+        this.xor(_images_file, params.url) ||
+        this.xor(_images_file, _parameters.url)
       )
     ) {
-      return _callback(err);
+      throw err;
     }
-    const _url = _parameters.url || params.url;
-    const _classifier_ids = _parameters.classifier_ids || params.classifier_ids;
-    const _owners = _parameters.owners || params.owners;
-    const _threshold = _parameters.threshold || params.threshold;
-    let _obj = {
-      url: _url,
-      classifier_ids: _classifier_ids,
-      owners: _owners,
-      threshold: _threshold
-    };
-    // remove null/undefined keys
-    Object.keys(_obj).forEach(key => {
-      _obj[key] == null && delete _obj[key];
+  }
+
+  private parseParameters(params) {
+    let parameters;
+    try {
+      parameters = JSON.parse(params.parameters);
+    } catch (e) {
+      parameters = {};
+    }
+    const parameters_keys = ['url', 'classifier_ids', 'owners', 'threshold'];
+    let _parameters = {};
+    parameters_keys.forEach(key => {
+      if (parameters[key] || params[key]) {
+        _parameters[key] = parameters[key] || params[key];
+      }
     });
-    return Object.keys(_obj).length > 0 ? _obj : null;
+    return Object.keys(_parameters).length > 0 ? _parameters : null;
   }
 
   classify(params, callback) {
-    const _parameters = this.parseParameters(params, callback) || {};
-    // set defaults for classify()
-    if (!_parameters.classifier_ids) {
-      _parameters.classifier_ids = ['default'];
+    const _callback = typeof callback === 'function' ? callback : () => {};
+    try {
+      this.checkVisionParams(params);
+    } catch (e) {
+      return _callback(e);
     }
-    if (!_parameters.owners) {
-      _parameters.owners = ['me', 'IBM'];
+    if (params.image_file) {
+      params.images_file = params.image_file;
     }
-    const _params = extend(params, { parameters: JSON.stringify(_parameters) });
-    return super.classify(_params, callback);
+    const defaultParameters = {
+      classifier_ids: ['default'],
+      owners: ['me', 'IBM']
+    };
+    const _parameters = extend(
+      {},
+      defaultParameters,
+      this.parseParameters(params)
+    );
+    const _params = extend(params, { parameters: _parameters });
+    return super.classify(_params, _callback);
   }
 
   detectFaces(params, callback) {
-    const _parameters = this.parseParameters(params, callback);
-    if (!_parameters) {
-      // if images_file
-      return super.detectFaces(params, callback);
-    } else {
-      const _params = extend(params, {
-        parameters: JSON.stringify(_parameters)
-      });
-      return super.detectFaces(_params, callback);
+    const _callback = typeof callback === 'function' ? callback : () => {};
+    try {
+      this.checkVisionParams(params);
+    } catch (e) {
+      return _callback(e);
     }
+    if (params.image_file) {
+      params.images_file = params.image_file;
+    }
+    const _params = extend({}, params, {
+      parameters: this.parseParameters(params)
+    });
+    return super.detectFaces(_params, _callback);
   }
 
   createClassifier(params, callback) {
     const _callback = callback ? callback : () => {};
+    const err = new Error(
+      'Missing required parameters: either two *_positive_examples' +
+        'or one *_positive_examples and one negative_examples are required'
+    );
     if (!params) {
-      return _callback(
-        new Error(
-          'Missing required parameters: either two *_positive_examples' +
-            'or one *_positive_examples and one negative_examples are required'
-        )
-      );
+      return _callback(err);
     }
     const example_keys = Object.keys(params).filter(key => {
       return key === 'negative_examples' || key.match(/^.+positive_examples$/);
     });
     if (example_keys.length < 2) {
-      return _callback(
-        new Error(
-          'Missing required parameters: either two *_positive_examples' +
-            'or one *_positive_examples and one negative_examples are required'
-        )
-      );
+      return _callback(err);
     }
     return super.createClassifier(params, _callback);
   }
