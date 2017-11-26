bbd0289e9866d8a0cd16d810490e260bcffc5198
Ammar Dodin <ammar.dodin@ibm.com>
2017-11-25 22:43:37 -0500

    :art: format tts adapter

--- text-to-speech/v1.ts
+++ text-to-speech/v1.ts
@@ -1,26 +1,25 @@
 import GeneratedTextToSpeechV1 = require('./v1-generated');
-import extend = require('extend')
 
 class TextToSpeechV1 extends GeneratedTextToSpeechV1 {
-    constructor(options) {
-        super(options);
-    }
+  constructor(options) {
+    super(options);
+  }
 
-    getCustomizations(params, callback) {
-        return super.listCustomizations(params, callback);
-    }
+  getCustomizations(params, callback) {
+    return super.listCustomizations(params, callback);
+  }
 
-    getWords(params, callback) {
-        return super.listWords(params, callback);
-    }
+  getWords(params, callback) {
+    return super.listWords(params, callback);
+  }
 
-    voices(params, callback) {
-        return super.listVoices(params, callback);
-    }
+  voices(params, callback) {
+    return super.listVoices(params, callback);
+  }
 
-    voice(params, callback) {
-        return super.getVoice(params, callback);
-    }
+  voice(params, callback) {
+    return super.getVoice(params, callback);
+  }
 }
 
 export = TextToSpeechV1;
