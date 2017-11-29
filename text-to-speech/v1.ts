0558f4e0b58a3bfbb007bb4da853b333b826a4a2
Ammar Dodin <ammar.dodin@ibm.com>
2017-11-29 11:24:50 -0500

    :new: add repairWavHeader helper to text to speech

--- text-to-speech/v1.ts
+++ text-to-speech/v1.ts
@@ -20,6 +20,62 @@ class TextToSpeechV1 extends GeneratedTextToSpeechV1 {
   voice(params, callback) {
     return super.getVoice(params, callback);
   }
+
+  /**
+   * Repair the WAV header of an audio/wav file.
+   *
+   * @param {Buffer} wavFileData - Wave audio - will be edited in place and returned
+   * @return {Buffer} wavFileData - the original Buffer, with a correct header
+   */
+  repairWavHeader = function(wavFileData) {
+    const totalBytes = wavFileData.length;
+
+    // bytes 4-8 in header give the total file size,
+    // after the first 8 bytes
+    // this is a reliable constant
+    const chunkSize = totalBytes - 8;
+    wavFileData.writeInt32LE(chunkSize, 4);
+
+    // the first subchunk is at byte 12, the fmt subchunk
+    // this is the only other reliable constant
+    let chunkIdOffset = 12;
+    const fieldSize = 4;
+
+    // every subchunk has a 4 byte id followed by a 4 byte size field
+    let chunkSizeOffset = chunkIdOffset + fieldSize;
+    let subchunk2sizeLocation = 0;
+
+    // initialize values to hold data of each chunk we come across
+    let tempChunkID = '';
+    let tempChunkSize = 0;
+
+    while (tempChunkID !== 'data') {
+      if (chunkSizeOffset + fieldSize > totalBytes) {
+        break;
+      }
+
+      tempChunkID = wavFileData
+        .slice(chunkIdOffset, chunkIdOffset + fieldSize)
+        .toString('ascii');
+      tempChunkSize = wavFileData.readInt32LE(chunkSizeOffset);
+
+      // save the location of the data size field
+      if (tempChunkID === 'data') {
+        subchunk2sizeLocation = chunkSizeOffset;
+      }
+
+      // skip over all the data in the temp chunk
+      chunkIdOffset = chunkSizeOffset + fieldSize + tempChunkSize;
+      chunkSizeOffset = chunkIdOffset + fieldSize;
+    }
+
+    const subchunk2size = totalBytes - subchunk2sizeLocation - fieldSize;
+
+    // update the size of the audio data and return
+    wavFileData.writeInt32LE(subchunk2size, subchunk2sizeLocation);
+
+    return wavFileData;
+  };
 }
 
 export = TextToSpeechV1;
