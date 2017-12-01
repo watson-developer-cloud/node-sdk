80e645308d34963bf2f0d30362c18e808b6a962b
German Attanasio <germanattanasio@gmail.com>
2017-11-30 22:47:28 -0500

    :fire: remove readFileSync usage

--- lib/requestwrapper.ts
+++ lib/requestwrapper.ts
@@ -23,11 +23,8 @@ import {
   isEmptyObject
 } from './helper';
 import { PassThrough as readableStream } from 'stream';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 
-const pkgPath = join(__dirname, '../package.json');
-const pkg = JSON.parse(readFileSync(pkgPath).toString('utf-8'));
+const pkgPath = require('../package.json');
 const isBrowser = typeof window === 'object';
 
 /**
