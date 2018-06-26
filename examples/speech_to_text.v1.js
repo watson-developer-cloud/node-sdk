5348000edc68c37b3b6eaee0c50895a5119d342c
Dustin Popp <dpopp07@gmail.com>
2018-06-26 16:58:16 -0500

    address pr comments on stt example

--- examples/speech_to_text.v1.js
+++ examples/speech_to_text.v1.js
@@ -12,7 +12,7 @@ var speechToText = new SpeechToTextV1({
     This code will print the entire response to the console when it
     receives the 'data' event. Some applications will want to write
     out only the transcribed text, to the console or to a file.
-    To this, remove `objectMode: true` from the `params` object.
+    To do this, remove `objectMode: true` from the `params` object.
     Then, uncomment the block of code at Line 30.
 */
 
@@ -28,11 +28,12 @@ var recognizeStream = speechToText.createRecognizeStream(params);
 fs.createReadStream(__dirname + '/resources/speech.wav').pipe(recognizeStream);
 
 /*
+// these two lines of code will only work if `objectMode` is `false`
+
 // pipe out the transcription to a file
 recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
 
 // get strings instead of Buffers from `data` events
-// only do this when objectMode is `false`
 recognizeStream.setEncoding('utf8');
 */
 
