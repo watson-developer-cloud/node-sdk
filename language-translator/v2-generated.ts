99fc35ebab7acee727068db83c8b1a3ada23c665
Logan Patino <loganpatino10@gmail.com>
2018-06-29 09:25:30 -0400

    chore(Language Translator):Add deprecation notices for Language Translator v2 (#735)
    This PR adds deprecation notices for Language Translator v2 in the README and service class.
    
    This will also be the final change for release 3.5.2.

--- language-translator/v2-generated.ts
+++ language-translator/v2-generated.ts
@@ -22,6 +22,8 @@ import { FileObject } from '../lib/helper';
 
 /**
  * IBM Watson&trade; Language Translator translates text from one language to another. The service offers multiple domain-specific models that you can customize based on your unique terminology and language. Use Language Translator to take news from across the globe and present it in your language, communicate with your customers in their own language, and more.
+ * 
+ * @deprecated Language Translator v3 is now available. The v2 Language Translator API will no longer be available after July 31, 2018. To take advantage of the latest service enhancements, migrate to the v3 API. View the [Migrating to Language Translator v3](https://console.bluemix.net/docs/services/language-translator/migrating.html) page for more information.
  */
 
 class LanguageTranslatorV2 extends BaseService {
