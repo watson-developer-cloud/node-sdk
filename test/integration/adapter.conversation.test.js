118ff7f67c947cc99a76cd8116df151844920777
Dustin Popp <dpopp07@gmail.com>
2019-03-13 10:24:24 -0500

    test: update conversation/assistant tests to match service changes

--- test/integration/adapter.conversation.test.js
+++ test/integration/adapter.conversation.test.js
@@ -248,7 +248,7 @@ describe('conversation_adapter_integration', function() {
         workspace1.workspace_id = result.workspace_id;
         expect(result.name).toBe(params.name);
         expect(result.language).toBe('fr');
-        expect(result.metadata).toBe(params.metadata);
+        expect(result.metadata).toBeDefined();
         expect(result.description).toBe(params.description);
         done();
       });
@@ -265,7 +265,7 @@ describe('conversation_adapter_integration', function() {
         }
         expect(result.name).toBe(params.name);
         expect(result.language).toBe('fr');
-        expect(result.metadata).toBe(params.metadata);
+        expect(result.metadata).toBeDefined();
         expect(result.description).toBe(params.description);
         done();
       });
