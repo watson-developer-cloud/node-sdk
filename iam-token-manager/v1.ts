4376e7c2932050aaea163eb8721c30c84f7b04ca
Dustin Popp <dpopp07@gmail.com>
2019-03-19 11:21:38 -0500

    fix: expose token manager from core code as a module

---
+++ iam-token-manager/v1.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @module iam-token-manager-v1
+ */
+
+import { IamTokenManagerV1 } from 'ibm-cloud-sdk-core/iam-token-manager/v1';
+export = IamTokenManagerV1;
