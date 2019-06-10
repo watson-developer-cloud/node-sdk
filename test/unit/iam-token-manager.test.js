bca5f4070f3f3a2efd382afc3f90ca9ca3b4f0e7
Dustin Popp <dpopp07@gmail.com>
2019-06-10 10:53:37 -0500

    test: authorization tests fixed

--- test/unit/iam-token-manager.test.js
+++ test/unit/iam-token-manager.test.js
@@ -5,7 +5,7 @@ describe('iam token manager', () => {
   it('should correctly export the token manager from the core module', () => {
     const tokenManager = new TokenManagerV1({});
     expect(tokenManager).toBeDefined();
-    expect(tokenManager.iamUrl).toBeDefined();
+    expect(tokenManager.url).toBeDefined();
     expect(tokenManager.tokenInfo).toBeDefined();
     expect(tokenManager.getToken).toBeInstanceOf(Function);
     expect(tokenManager.setAccessToken).toBeInstanceOf(Function);
