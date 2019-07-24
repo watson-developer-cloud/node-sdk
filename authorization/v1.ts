ca8135f6741f0639a085cf11c149f94291187b8b
Dustin Popp <dpopp07@gmail.com>
2019-07-24 16:03:07 -0500

    docs: fix copyright dates

--- authorization/v1.ts
+++ authorization/v1.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 IBM Corp. All Rights Reserved.
+ * (C) Copyright IBM Corp. 2015, 2019.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- personality-insights/v3.ts
+++ personality-insights/v3.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 IBM All Rights Reserved.
+ * (C) Copyright IBM Corp. 2017, 2019.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
