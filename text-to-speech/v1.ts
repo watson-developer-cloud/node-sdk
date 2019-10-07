42b0913ac44314bd1baf574c23d1f570e953dbbd
Dustin Popp <dpopp07@gmail.com>
2019-10-07 13:44:44 -0500

    feat(text-to-speech): add method to repair wav header for a stream (#981)
    * the method takes a stream of wav data, reads it into memory, repairs the header, then returns a Promise that resolves with the repaired data as a buffer

--- text-to-speech/v1.ts
+++ text-to-speech/v1.ts
@@ -1,6 +1,8 @@
 import extend = require('extend');
 import { OutgoingHttpHeaders } from 'http';
 import { UserOptions } from 'ibm-cloud-sdk-core';
+import isStream = require('isstream');
+import { Readable } from 'stream';
 import { getSdkHeaders } from '../lib/common';
 import SynthesizeStream = require('../lib/synthesize-stream');
 import GeneratedTextToSpeechV1 = require('./v1-generated');
@@ -11,10 +13,42 @@ class TextToSpeechV1 extends GeneratedTextToSpeechV1 {
   }
 
   /**
+   * Repair the WAV header of an audio/wav file in Stream format.
+   * The Stream is read into memory, then the data is repaired and returned as a Buffer.
+   *
+   * @param {Buffer} wavFileAsStream - wave audio as a stream
+   * @return {Buffer} wavFileData - a Buffer with the correct header
+   */
+  repairWavHeaderStream = (wavFileAsStream: Readable): Promise<Buffer> => {
+    // in case of accidentally calling the wrong method
+    if (!isStream(wavFileAsStream)) {
+      return Buffer.isBuffer(wavFileAsStream)
+        ? Promise.resolve(this.repairWavHeader(wavFileAsStream))
+        : Promise.reject('Expected input data to be a Stream.');
+    }
+
+    const buffers: Buffer[] = [];
+    return new Promise((resolve, reject) => {
+      // stream info to the buffer
+      wavFileAsStream.on('data', data => {
+        buffers.push(data);
+      });
+
+      wavFileAsStream.on('end', () => {
+        resolve(this.repairWavHeader(Buffer.concat(buffers)));
+      });
+
+      wavFileAsStream.on('error', err => {
+        reject(err);
+      });
+    })
+  };
+
+  /**
    * Repair the WAV header of an audio/wav file.
    *
    * @param {Buffer} wavFileData - Wave audio - will be edited in place and returned
-   * @return {Buffer} wavFileData - the original Buffer, with a correct header
+   * @return {Buffer} the original Buffer, with the correct header
    */
   repairWavHeader = (wavFileData: Buffer): Buffer => {
     const totalBytes = wavFileData.length;
