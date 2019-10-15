5438fd78bf1ec74bb5098cf14ff23ecf52152c0c
Shuyu Chen <shuyuchen@ibm.com>
2019-10-15 16:50:34 -0500

    feat: fixed side bar navigation to exclude interfaces and type aliases

--- scripts/typedoc/theme/helpers/excluded-nav-url.js
+++ scripts/typedoc/theme/helpers/excluded-nav-url.js
@@ -1,17 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function excludedNavUrl(cssString) {
+function excludedNavUrl(cssString, currentlySelected=false) {
     // We do not want to clutter documentation with interfaces, Type aliases and Modules
     // So we'll hide navigation links to those documentations
-    // This function will identify whether a url by its CSS class
-    // Returns true if type of URL is excluded. Returns false otherwise
+    // This function will identify a url by its CSS class
 
-    const excludedCssClasses = ['tsd-kind-interface', 'tsd-kind-type-alias']
-    const cssClasses = cssString.split(' ')
+    // We will make an exception for when excluded items are selected
+    // In that case, we do render the selected excluded URL in the side navigation
+    // Regardless of its other css properties
 
+    // Returns true to exclude something, return false otherwise
 
-    return cssClasses.some((cssClass) => {
-        return excludedCssClasses.includes(cssClass)
-    })
+    if (currentlySelected) {
+        // We do not want to exclude selected items
+        return false
+    } else {
+        const excludedCssClasses = ['tsd-kind-interface', 'tsd-kind-type-alias']
+        const cssClasses = cssString.split(' ')
+
+        return cssClasses.some((cssClass) => {
+            return excludedCssClasses.includes(cssClass)
+        })
+    }
 }
 exports.excludedNavUrl = excludedNavUrl;
