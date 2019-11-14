bb59a0ce1aea72ec30cb451d1e176a002bf732c2
Dustin Popp <dpopp07@gmail.com>
2019-11-14 08:56:01 -0600

    docs: update auth example file to use correct format (#997)

--- test/resources/auth.example.js
+++ test/resources/auth.example.js
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corp. 2015, 2018.
+ * Copyright 2015, 2019 IBM Corp. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,160 +16,54 @@
 
 'use strict';
 
+const testingHeaders = {
+  'X-Watson-Learning-Opt-Out': 1,
+  'X-Watson-Test': 1,
+};
+
 module.exports = {
-  relationship_extraction: {
-    url: 'https://gateway.watsonplatform.net/relationship-extraction-beta/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  concept_insights: {
-    url: 'https://gateway.watsonplatform.net/concept-insights/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-    account_id: 'larmu8up2pa3',
-  },
-  speech_to_text: {
-    url: 'https://stream.watsonplatform.net/speech-to-text/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  text_to_speech: {
-    url: 'https://stream.watsonplatform.net/text-to-speech/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  tradeoff_analytics: {
-    url: 'https://gateway.watsonplatform.net/tradeoff-analytics/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  visual_recognition: {
-    v3: {
-      url: 'https://gateway-a.watsonplatform.net/visual-recognition/api',
-      api_key: '',
-      headers: {
-        'X-Watson-Learning-Opt-Out': 1,
-        'X-Watson-Test': 1,
-      },
-    },
-  },
-  personality_insights: {
-    v2: {
-      url: 'https://gateway.watsonplatform.net/personality-insights/api',
-      username: '',
-      password: '',
-      headers: {
-        'X-Watson-Learning-Opt-Out': 1,
-        'X-Watson-Test': 1,
-      },
-    },
-    v3: {
-      url: 'https://gateway.watsonplatform.net/personality-insights/api',
-      username: '',
-      password: '',
-      headers: {
-        'X-Watson-Learning-Opt-Out': 1,
-        'X-Watson-Test': 1,
-      },
-    },
-  },
-  dialog: {
-    url: 'https://gateway.watsonplatform.net/dialog/api',
-    username: '',
-    password: '',
-    dialog_id: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  language_translator: {
-    url: 'https://gateway.watsonplatform.net/language-translator/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  tone_analyzer: {
-    url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  alchemy: {
-    api_key: '',
-  },
-  document_conversion: {
-    url: 'https://gateway.watsonplatform.net/document-conversion/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  retrieve_and_rank: {
-    url: 'https://gateway.watsonplatform.net/retrieve-and-rank/api',
-    username: '',
-    password: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
-  natural_language_classifier: {
-    url: 'https://gateway.watsonplatform.net/natural-language-classifier/api',
-    username: '',
-    password: '',
-    classifier_id: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
-  },
   assistant: {
-    url: 'https://gateway.watsonplatform.net/assistant/api',
-    username: '',
-    password: '',
-    workspace_id: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
+    headers: testingHeaders,
+    apikey: '',
+  },
+  compareComply: {
+    apikey: '',
+    headers: testingHeaders,
   },
   discovery: {
-    username: '',
-    password: '',
-    environment_id: '',
-    configuration_id: '',
-    headers: {
-      'X-Watson-Learning-Opt-Out': 1,
-      'X-Watson-Test': 1,
-    },
+    apikey: '',
+    headers: testingHeaders,
+  },
+  languageTranslator: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  naturalLanguageClassifier: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  naturalLanguageUnderstanding: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  personalityInsights: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  speechToText: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  textToSpeech: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  toneAnalyzer: {
+    apikey: '',
+    headers: testingHeaders,
+  },
+  visualRecognition: {
+    apikey: '',
+    headers: testingHeaders,
   },
 };
