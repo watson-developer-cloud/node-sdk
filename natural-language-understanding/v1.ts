93e7b6946ae92bc0499097bdfb92153509b924a2
Ajiemar Santiago <ajiemar@gmail.com>
2020-01-17 15:13:41 -0600

    fix(Natural Language Understanding v1): Restore model field in CategoriesOptions

--- natural-language-understanding/v1.ts
+++ natural-language-understanding/v1.ts
@@ -454,6 +454,16 @@ namespace NaturalLanguageUnderstandingV1 {
     explanation?: boolean;
     /** Maximum number of categories to return. */
     limit?: number;
+    /** Enter a [custom
+     *  model](https://cloud.ibm.com/docs/services/natural-language-understanding?topic=natural-language-understanding-customizing)
+     *  ID to override the standard categories model.
+     * 
+     *  The custom categories experimental feature will be retired on 19 December 2019. On that date, deployed custom
+     *  categories models will no longer be accessible in Natural Language Understanding. The feature will be removed
+     *  from Knowledge Studio on an earlier date. Custom categories models will no longer be accessible in Knowledge
+     *  Studio on 17 December 2019.
+     */
+    model?: string;
   }
 
   /** Relevant text that contributed to the categorization. */
