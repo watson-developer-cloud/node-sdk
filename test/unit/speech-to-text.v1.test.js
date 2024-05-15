6c79d93e8f4e57422238a4076eb90fe49c9f14bf
Angelo Paparazzi <angelo.paparazzi@ibm.com>
2024-05-15 13:35:46 -0500

    feat(stt): remove interimResults and lowLatency wss params

--- test/unit/speech-to-text.v1.test.js
+++ test/unit/speech-to-text.v1.test.js
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corp. 2024.
+ * (C) Copyright IBM Corp. 2018, 2024.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
